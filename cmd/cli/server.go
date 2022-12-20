b8ed67f3ddd8d577ab1865f4a4165f588da13e3b
Imre Nagi <imre.nagi@gojek.com>
2022-12-20 10:58:27 -0500

    create fs server
    Signed-off-by: Imre Nagi <imre.nagi@gojek.com>

--- cmd/cli/server.go
+++ cmd/cli/server.go
@@ -1,13 +1,14 @@
 package cli
 
 import (
-	"bufio"
+	"context"
 	"fmt"
-	"io"
-	"net"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/imrenagi/imrefs"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
 
@@ -23,54 +24,27 @@ func serverCmd() *cobra.Command {
 				return fmt.Errorf("invalid args length. should only accept one argument")
 			}
 			name := args[0]
-			fileName := fmt.Sprintf("files/file-%s.tmp", name)
-			l, err := net.Listen("unix", fmt.Sprintf("files/file-%s.sock", name))
-			if err != nil {
+
+			ctx := context.Background()
+			ctx, cancel := context.WithCancel(ctx)
+
+			ch := make(chan os.Signal, 1)
+			signal.Notify(ch, os.Interrupt)
+			signal.Notify(ch, syscall.SIGTERM)
+
+			go func() {
+				oscall := <-ch
+				log.Warn().Msgf("system call:%+v", oscall)
+				cancel()
+			}()
+
+			f := imrefs.New(name)
+			if err := f.Run(ctx); err != nil {
 				return err
 			}
 
-			for {
-				conn, err := l.Accept()
-				if err != nil {
-					return err
-				}
-
-				go handleConn(conn, fileName)
-			}
 			return nil
 		},
 	}
 	return command
-}
-
-func handleConn(conn net.Conn, fileName string) {
-	req, err := imrefs.ReadRequest(bufio.NewReader(conn))
-	if err != nil {
-		conn.Write([]byte("FAILED"))
-		return
-	}
-
-	switch req.Command {
-	case imrefs.IPC_SEND:
-		writeToFile(conn, fileName, req.Content)
-	case imrefs.IPC_STOP:
-		// TODO properly stop server
-		conn.Write([]byte("OK"))
-		os.Exit(0)
-	}
-}
-
-func writeToFile(conn net.Conn, name string, content io.ReadCloser) error {
-	f, err := os.OpenFile(name, os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0777)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	_, err = io.Copy(f, content)
-	if err != nil {
-		return nil
-	}
-	conn.Write([]byte("OK"))
-	return nil
 }
--- imrefs.go
+++ imrefs.go
@@ -1,1 +1,104 @@
 package imrefs
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"os"
+
+	"github.com/rs/zerolog/log"
+)
+
+var sockFormat = "files/file-%s.sock"
+var fileFormat = "files/file-%s.tmp"
+
+type FS struct {
+	name string
+
+	stopCh chan error
+}
+
+func New(name string) *FS {
+	fs := &FS{
+		name:   name,
+		stopCh: make(chan error),
+	}
+	return fs
+}
+
+func (f *FS) fileName() string {
+	return fmt.Sprintf(fileFormat, f.name)
+}
+
+func (f *FS) Run(ctx context.Context) error {
+	l, err := net.Listen("unix", fmt.Sprintf(sockFormat, f.name))
+	if err != nil {
+		return err
+	}
+	defer l.Close()
+
+	log.Info().Msg("listener created")
+
+	go func() {
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				f.stopCh <- err
+				return
+			}
+			go f.handleConn(conn)
+		}
+	}()
+
+	log.Info().Msg("listener run")
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case err := <-f.stopCh:
+		return err
+	}
+}
+
+func (f *FS) handleConn(conn net.Conn) {
+	log.Debug().Msg("handling connection")
+
+	req, err := ReadRequest(bufio.NewReader(conn))
+	if err != nil {
+		conn.Write([]byte("FAILED"))
+		return
+	}
+
+	log.Debug().Msg("get the request")
+
+	switch req.Command {
+	case IPC_SEND:
+		f.write(conn, req.Content)
+	case IPC_STOP:
+		// TODO properly stop server
+		conn.Write([]byte("OK"))
+		os.Exit(0)
+	}
+}
+
+func (f *FS) write(conn net.Conn, content io.ReadCloser) error {
+
+	file, err := os.OpenFile(f.fileName(), os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0777)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, content)
+	if err != nil {
+		return nil
+	}
+	conn.Write([]byte("OK"))
+	return nil
+}
+
+func (f *FS) stop() {
+	f.stopCh <- nil
+}
